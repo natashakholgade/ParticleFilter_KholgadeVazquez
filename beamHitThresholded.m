ed70db8de860157ee3864eda35f8c5379be64fa2
Marynel Vazquez <mvumail@gmail.com>
2011-10-05 13:59:45 -0400

    forgot to add beamhitthresholded

---
+++ beamHitThresholded.m
@@ -0,0 +1,20 @@
+% Thresholded hit location in world coordinates
+% P = beamPosition(X, L, d)
+%   particles       3xN           - particles in the world
+%   laser           1xL           - laser readings (in cm)
+%   minT                          - min allowed laser reading 
+%   maxT                          - max allowed laser reading
+%   stride                        - laser array stride (> 0)
+%   offset                        - laser (forward) offset
+%   hits            BxNx2         - laser x,y position (in cm)
+% where N = # particles and B = 1 + ((L-1)/stride)
+%
+% If a range measurement is outside [minT, maxT], then NaN is returned for
+% the [x,y] hit coordinate. See beamHit() for an explanation about all
+% the other parameters.
+function hits = beamHitThresholded(particles, laser, minT, maxT, ...
+                                    stride, offset)
+                                
+laser((laser < minT)) = NaN;
+laser((laser > maxT)) = NaN;
+hits = beamHit(particles, laser, stride, offset);
