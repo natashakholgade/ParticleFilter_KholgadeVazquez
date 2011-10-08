6d29257f21114afe17acf26f22e2c3f785389cbc
Marynel Vazquez <mvumail@gmail.com>
2011-10-07 23:54:15 -0400

    added visAllLaserData.m and visLaserDistribution.m to visualize laser range data as in the book (Fig. 6.5, page 159)

---
+++ visLaserDistribution.m
@@ -0,0 +1,10 @@
+% Visualize laser data distribution
+%   measurements LxB  - L measurements, each composed by B beams
+%   xrange       1x2  - x coordinate range for scatter plot (e.g., [1,LxB])      
+%   color             - scatter plot color
+function visLaserDistribution(measurements, xrange, color)
+
+[L, B] = size(measurements);
+measurements = reshape(measurements,1,L*B);
+
+scatter(xrange(1):xrange(2), measurements, 1, color);
