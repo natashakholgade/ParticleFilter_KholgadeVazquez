a803cbbb61dfe6767d4caf0f688551c36f9aceae
Natasha Kholgade <natasha.kholgade@gmail.com>
2011-10-08 21:47:34 -0400

    weightsFromMap

--- weightsFromMap.m
+++ weightsFromMap.m
@@ -17,6 +17,7 @@ hits(invalidHits) = 1;                      % replace NaN by 1 so sub2ind works
 % Fix beams that fall outside map
 x = hits(:,:,1);
 y = hits(:,:,2);
+outside = (x < 1) | (x > mapSize(2)) | (y < 1) | (y > mapSize(1));
 x(x < 1) = 1; x(x > mapSize(2)) = mapSize(2);
 y(y < 1) = 1; y(y > mapSize(1)) = mapSize(1);
 hits(:,:,1) = x; hits(:,:,2) = y;
@@ -28,12 +29,17 @@ hitProb = map(sub2ind(mapSize, hits(:,:,2), hits(:,:,1)));
 % (sum a little number to cells with 0 prob so that the weight is not zero 
 % due to few measurement/map inconsistencies)
 epsilon = 0.0001;
+hitProb(outside) = epsilon;
 hitProb(invalidHits(:,:,1)) = 1;
-hitProb(hitProb == 2) = epsilon;
+hitProb(hitProb == 2) = 0; %epsilon;
 hitProb(hitProb < epsilon) = epsilon;
 
 % Compute joint prob from all beams per particle 
-W = prod(hitProb);
+%W = exp(prod(hitProb));
+%W=prod(exp(hitProb));
+%W=prod(hitProb.^(1/200));
+%W=prod(hitProb);
+W=sum(hitProb);
 
 % Normalize weights
 W = W'./sum(W);
